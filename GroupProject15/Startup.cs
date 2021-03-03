f390de963c3c27e5dc9fce34e0981fbd2e420116
CosmicRay11 <george.w11@hotmail.co.uk>
2021-03-03 15:30:55 +0000

    Test commit

--- GroupProject15/Startup.cs
+++ GroupProject15/Startup.cs
@@ -9,6 +9,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+//comment to check github works
+
 namespace GroupProject15
 {
     public class Startup
@@ -16,6 +18,7 @@ namespace GroupProject15
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            Console.WriteLine("Hello there");
         }
 
         public IConfiguration Configuration { get; }
