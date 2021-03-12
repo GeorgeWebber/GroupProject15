325d9a9343612a9192a3c130e3942608e147382b
CosmicRay11 <george.w11@hotmail.co.uk>
2021-03-12 16:13:28 +0000

    Added validation to some fields in the positive case form

--- GroupProject15/Pages/PositiveForm.cshtml.cs
+++ GroupProject15/Pages/PositiveForm.cshtml.cs
@@ -6,6 +6,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+//TODO full validation on all fields
+//TODO add rest of fields
+
 namespace GroupProject15.Pages
 {
     public class PositiveFormModel : PageModel
@@ -33,12 +36,15 @@ namespace GroupProject15.Pages
 
         public int Id { get; set; }
 
+        
         [BindProperty, Required(ErrorMessage = "Please supply a forename"), Display(Name = "Forename")]
         public string Forename { get; set; }
 
         [BindProperty, Required(ErrorMessage = "Please supply a last name"), Display(Name = "Last name")]
         public string LastName { get; set; }
 
+        //TODO check this regex
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Please supply a valid email address")]
         [BindProperty, Required(ErrorMessage = "Please supply a valid email address"), Display(Name = "Email address")]
         public string EmailAddress { get; set; }
         
