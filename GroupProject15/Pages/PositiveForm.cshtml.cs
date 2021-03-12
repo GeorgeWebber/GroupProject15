5ba5a23d62eec1c8544d9e3d6f27e2872db1ea31
CosmicRay11 <george.w11@hotmail.co.uk>
2021-03-12 14:22:52 +0000

    Added a basic form page without database support. Has existence validation. Added a page to show when a form is successfully entered

--- GroupProject15/Pages/PositiveForm.cshtml.cs
+++ GroupProject15/Pages/PositiveForm.cshtml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 //using GroupProject15.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,10 +13,36 @@ namespace GroupProject15.Pages
         public void OnGet()
         {
         }
+        private readonly ILogger<PositiveFormModel> _logger;
+
+        public PositiveFormModel(ILogger<PositiveFormModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            //Product = await db.Products.FindAsync(Id);  some sort of await command here, so this runs when a command succeeds
+            if (ModelState.IsValid)
+            {
+                return RedirectToPage("PositiveFormSuccess");
+            }
+            return Page();
+        }
+
 
         public int Id { get; set; }
-        [BindProperty, Id, Required, Display (Name ="Id")]
+
+        [BindProperty, Required(ErrorMessage = "Please supply a forename"), Display(Name = "Forename")]
         public string Forename { get; set; }
+
+        [BindProperty, Required(ErrorMessage = "Please supply a last name"), Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [BindProperty, Required(ErrorMessage = "Please supply a valid email address"), Display(Name = "Email address")]
         public string EmailAddress { get; set; }
+        
+        
     }
+
 }
