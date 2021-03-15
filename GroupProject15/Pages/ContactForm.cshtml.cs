13bc26c95772544f3e38cbdeb5e559338af8ed1a
Tyler Lawrence <ty.mikel7@googlemail.com>
2021-03-15 13:38:00 +0000

    dfg

--- GroupProject15/Pages/ContactForm.cshtml.cs
+++ GroupProject15/Pages/ContactForm.cshtml.cs
@@ -6,27 +6,24 @@ using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-//TODO full validation on all fields
-//TODO add rest of fields
-
 namespace GroupProject15.Pages
 {
-    public class PositiveFormModel : PageModel
+    public class ContactFormModel : PageModel
     {
         public void OnGet()
         {
         }
+
         private readonly ILogger<PositiveFormModel> _logger;
 
-        public PositiveFormModel(ILogger<PositiveFormModel> logger)
+        public ContactFormModel(ILogger<PositiveFormModel> logger)
         {
             _logger = logger;
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            //Product = await db.PositiveCases.FindAsync(Id);  some sort of await command here, so this runs when a command succeeds
-            Console.WriteLine("iauysd");
+            //Product = await db.Products.FindAsync(Id);  some sort of await command here, so this runs when a command succeeds
             if (ModelState.IsValid)
             {
                 return RedirectToPage("PositiveFormSuccess");
@@ -34,10 +31,8 @@ namespace GroupProject15.Pages
             return Page();
         }
 
-
         public int Id { get; set; }
 
-        
         [BindProperty, Required(ErrorMessage = "Please supply a forename"), Display(Name = "Forename")]
         public string Forename { get; set; }
 
@@ -45,17 +40,8 @@ namespace GroupProject15.Pages
         public string LastName { get; set; }
 
         //TODO check this regex
-        //[RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Please supply a valid email address")]
-        [EmailAddress]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Please supply a valid email address")]
         [BindProperty, Required(ErrorMessage = "Please supply a valid email address"), Display(Name = "Email address")]
         public string EmailAddress { get; set; }
-        
-        [Phone]
-        [BindProperty, Required(ErrorMessage = "Please supply a phone number"), Display(Name = "Phone Number")]
-        public string PhoneNumber { get; set; }
-
-        [BindProperty, Required(ErrorMessage = "Please enter date when sample was taken"), Display(Name = "Date of Test")]
-        public DateTime TestDate { get; set; }
     }
-
 }
